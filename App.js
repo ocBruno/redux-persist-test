4ec096ac041716e223013714a7053ee69e719411
Jeff Lewis <jeffylew77@gmail.com>
2019-06-26 17:14:41 -0700

    [Dev] Adding Redux Persist

--- App.js
+++ App.js
@@ -1,19 +1,55 @@
+// Imports: Dependencies
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { AsyncStorage, StyleSheet, Text, View } from 'react-native';
+import { persistStore } from 'redux-persist';
+import { Provider } from 'react-redux';
+import store from './store/store';
 
+// Imports: Screens
+import Counter from './screens/Counter';
+
+// React Native: App
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  state = {
+    isReady: false,
+  }
+
+  // Component Did Mount
+  componentDidMount = () => {
+    try {
+      // Redux Persist: 
+      persistStore(
+        store,
+        {
+          // Storage Method (React Native)
+          storage: AsyncStorage,
+          // Whitelist (Save Specific Reducers)
+          whitelist: [
+            'authReducer',
+          ],
+          // Blacklist (Don't Save Specific Reducers)
+          blacklist: [
+            'counterReducer',
+          ],
+        },
+        () => {
+          this.setState({
+            isReady: true,
+          })
+        }
+      )
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+
+
+  return (
+    // Redux: Global Store
+    <Provider store={store}>
+      <Counter />
+    </Provider>
+  );
+};
